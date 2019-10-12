2911093b5686afcd0cdd2539bf1df8d66316ddc9
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2019-10-12 13:54:24 -0800

    Cleanup the main method by pulling out coded into their own methods.

--- BeadedStream_HON/Program.cs
+++ BeadedStream_HON/Program.cs
@@ -7,12 +7,52 @@ namespace BeadedStream_HON
 {
     class Program
     {
+        private static bool debug = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Download the details.xml file
+            IRestResponse response = GetXMLFile("http://169.254.1.1/details.xml");
 
+            // Convert XML to JSON
+            string json= XMLtoJSON(response.Content);
 
-            var client = new RestClient("http://169.254.1.1/details.xml");
+            // Correct JSON to remove unneeded XML and dashes
+            // We are working with single quotes to avoid escaping each double quote
+            json = SingleToDboule(json, "'?xml':{'@version':'1.0','@encoding':'UTF-8'},", "");
+            json = SingleToDboule(json, "'Devices-Detail-Response'", "'DevicesDetailResponse'");
+            json = SingleToDboule(json, "'@xmlns':'http://beadedstream.com/schema/netgate','@xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance',", "");
+            json = SingleToDboule(json, ":{'@Units':", ":{'Units':");
+            json = SingleToDboule(json, ",'#text':", ",'text':");
+            json = SingleToDboule(json, "{'@Description':", "{'Description':");
+
+            // Parse JSON for elements
+            //var jsonObject = JsonConvert.SerializeXmlNode(doc);
+            RootObject wireData = JsonConvert.DeserializeObject<RootObject>(json);
+
+            // Get the temperature
+            foreach(OwdDS18B20 sensor in wireData.DevicesDetailResponse.owd_DS18B20)
+            {
+                Console.WriteLine(sensor.SensorID + ": " + sensor.TemperatureCalibrated);
+            }
+
+        }
+
+        private static string XMLtoJSON(string xmlData)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlData);
+            string json = JsonConvert.SerializeXmlNode(doc);
+
+            if (debug)
+                Console.WriteLine(json);
+
+            return json;
+        }
+
+        private static IRestResponse GetXMLFile(string xmlURL = "http://169.254.1.1/details.xml")
+        {
+            var client = new RestClient(xmlURL);
             var request = new RestRequest(Method.GET);
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("Connection", "keep-alive");
@@ -25,38 +65,13 @@ namespace BeadedStream_HON
             request.AddHeader("Content-Type", "application/json");
             IRestResponse response = client.Execute(request);
 
-            Console.WriteLine(response.Content);
+            if (debug)
+                Console.WriteLine(response.Content);
 
-            // Convert XML to JSON
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(response.Content);
-
-            string json = JsonConvert.SerializeXmlNode(doc);
-            Console.WriteLine(json);
-
-            // correct JSON to remove unneeded XML and dashes
-            // We are working with single quotes to avoid escaping
-            json = singleToDboule(json, "'?xml':{'@version':'1.0','@encoding':'UTF-8'},", "");
-            json = singleToDboule(json, "'Devices-Detail-Response'", "'DevicesDetailResponse'");
-            json = singleToDboule(json, "'@xmlns':'http://beadedstream.com/schema/netgate','@xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance',", "");
-            json = singleToDboule(json, ":{'@Units':", ":{'Units':");
-            json = singleToDboule(json, ",'#text':", ",'text':");
-            json = singleToDboule(json, "{'@Description':", "{'Description':");
-            //json = singleToDboule(json, "", "");
-
-            Console.WriteLine("");
-            Console.WriteLine(json);
-
-            // Parse JSON for elements
-            //var jsonObject = JsonConvert.SerializeXmlNode(doc);
-            RootObject account = JsonConvert.DeserializeObject<RootObject>(json);
-
-            Console.WriteLine("Hold");
-            // Get the temperature
-
+            return response;
         }
 
-        private static string singleToDboule(string originalString, string badXMLSingle, string replaceXMLSingle)
+        private static string SingleToDboule(string originalString, string badXMLSingle, string replaceXMLSingle)
         {
             // Replace single quote with double quote
             string badXMLDouble = badXMLSingle.Replace("'", "\"");
